f9990649f2974ad1a3fc47b1133bf8ff7bdd2498
Jorge Ferrero Linacero <jorgeferrerolinacero@gmail.com>
2022-09-09 21:45:01 +0200

    just create new handler

--- go.mod
+++ go.mod
@@ -3,3 +3,15 @@ module github.com/jferrl/my-merche
 go 1.18
 
 require github.com/yanzay/tbot/v2 v2.2.0
+
+require (
+	github.com/jferrl/go-merche v1.2.0
+	golang.org/x/oauth2 v0.0.0-20220909003341-f21342109be1
+)
+
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	golang.org/x/net v0.0.0-20220624214902-1bab6f366d9e // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,13 +1,27 @@
 package main
 
 import (
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
+	"net/url"
 	"os"
+	"strings"
 
 	"github.com/yanzay/tbot/v2"
 )
 
-var ttoken = os.Getenv("TELEGRAM_TOKEN")
+var (
+	port = os.Getenv("PORT")
+
+	ttoken = os.Getenv("TELEGRAM_TOKEN")
+
+	clientID     = os.Getenv("MERCEDES_CLIENT_ID")
+	clientSecret = os.Getenv("MERCEDES_CLIENT_SECRET")
+)
 
 func main() {
 	bot := tbot.New(ttoken)
@@ -15,8 +29,75 @@ func main() {
 	bot.HandleMessage("/info", func(m *tbot.Message) {
 		c.SendMessage(m.Chat.ID, "hello!")
 	})
+	bot.HandleMessage("/authorize", func(m *tbot.Message) {
+		c.SendMessage(m.Chat.ID, "Open https://my-merche.herokuapp.com/login/mercedes")
+	})
 	err := bot.Start()
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	http.HandleFunc("/login/mercedes/", mercedesLoginHandler)
+	http.HandleFunc("/login/mercedes/callback", mercedesCallbackHandler)
+
+	log.Panic(
+		http.ListenAndServe(":"+port, nil),
+	)
+}
+
+func mercedesLoginHandler(w http.ResponseWriter, r *http.Request) {
+	redirectURL := fmt.Sprintf(
+		"https://id.mercedes-benz.com/as/authorization.oauth2?response_type=code&client_id=%v&redirect_uri=%v&scope=%v&state=%v",
+		clientID,
+		"https://my-merche.herokuapp.com/login/mercedes/callback",
+		"mb:vehicle:mbdata:fuelstatus",
+		"login",
+	)
+
+	http.Redirect(w, r, redirectURL, http.StatusTemporaryRedirect)
+}
+
+func mercedesCallbackHandler(w http.ResponseWriter, r *http.Request) {
+	code := r.URL.Query().Get("code")
+
+	getMercedesAccessToken(code)
+}
+
+func getMercedesAccessToken(code string) string {
+	authToken := base64.StdEncoding.EncodeToString([]byte(clientID + ":" + clientSecret))
+
+	data := url.Values{}
+	data.Set("grant_type", "authorization_code")
+	data.Set("redirect_uri", "https://my-merche.herokuapp.com/login/mercedes/callback")
+	data.Set("code", code)
+
+	req, err := http.NewRequest(
+		http.MethodPost,
+		"https://id.mercedes-benz.com/as/token.oauth2",
+		strings.NewReader(data.Encode()),
+	)
+	if err != nil {
+		log.Panic("Request creation failed")
+	}
+	req.Header.Set("Authorization", "Basic "+authToken)
+	req.Header.Set("content-type", "application/x-www-form-urlencoded")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Panic("Request failed")
+	}
+
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	type mercedesAccessTokenResponse struct {
+		AccessToken  string `json:"access_token"`
+		TokenType    string `json:"token_type"`
+		RefreshToken string `json:"refresh_token"`
+		ExpiresIn    int    `json:"expires_in"`
+	}
+
+	var res mercedesAccessTokenResponse
+	json.Unmarshal(body, &res)
+
+	return res.AccessToken
 }
