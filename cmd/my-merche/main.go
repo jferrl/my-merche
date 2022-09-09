e53fd8b66b6becf48ff1c4486f2e6b21a7756869
Jorge Ferrero Linacero <jorgeferrerolinacero@gmail.com>
2022-09-09 20:52:38 +0200

    chore: main scafolding

--- cmd/my-merche/main.go
+++ cmd/my-merche/main.go
@@ -1,5 +1,22 @@
-package mymerche
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/yanzay/tbot/v2"
+)
+
+var ttoken = os.Getenv("TELEGRAM_TOKEN")
 
 func main() {
-
+	bot := tbot.New(ttoken)
+	c := bot.Client()
+	bot.HandleMessage("/info", func(m *tbot.Message) {
+		c.SendMessage(m.Chat.ID, "hello!")
+	})
+	err := bot.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/jferrl/my-merche
 
 go 1.18
+
+require github.com/yanzay/tbot/v2 v2.2.0
